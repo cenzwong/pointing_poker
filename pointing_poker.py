2c194dba4656a3bf0225658976d616461d5d9866
laszloszolnoki <laszlo.szolnoki@gmail.com>
2023-11-09 17:13:21 +0100

    Update pointing_poker.py

--- pointing_poker.py
+++ pointing_poker.py
@@ -8,4 +8,10 @@ cur_sqlite = conn_sqlite.cursor()
 cur_sqlite.execute('create table if not exists test (1 int, b int)')
 conn_sqlite.commit()
 
+tt = pd.read_sql_query('select a from test', conn_sqlite)['a'].to_list()
+
+if len(tt) = 0 :
+  cur_sqlite.execute("""insert into test values ('1', '1')""")
+
+
 st.write('Hello habagya!')
