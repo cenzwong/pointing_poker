c1ae40e563bdb8d8f8b599e7846b581d77ad100d
laszloszolnoki <laszlo.szolnoki@gmail.com>
2023-11-09 17:10:32 +0100

    Update pointing_poker.py

--- pointing_poker.py
+++ pointing_poker.py
@@ -1,5 +1,11 @@
 import streamlit as st
 import sqlite3
+import pandas as pd
 
+conn_sqlite = sqlite3.connect('test.sqlite')
+cur_sqlite = conn_sqlite.cursor()
+
+cur_sqlite.execute('create table if not exists test (1 int, b int)')
+conn_sqlite.commit()
 
 st.write('Hello habagya!')
