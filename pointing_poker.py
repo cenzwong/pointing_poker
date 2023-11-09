aff6f2147c7932db64631be91fc17567b5e73f41
laszloszolnoki <laszlo.szolnoki@gmail.com>
2023-11-09 17:21:37 +0100

    Update pointing_poker.py

--- pointing_poker.py
+++ pointing_poker.py
@@ -12,9 +12,12 @@ tt = pd.read_sql_query('select * from test', conn_sqlite)
 ttt = tt['a'].to_list()
 
 if len(tt) == 0 :
-  cur_sqlite.execute("""insert into test values ('1', '1')""")
-  conn_sqlite.commit()
-
+    cur_sqlite.execute("""insert into test values ('1', '1')""")
+    conn_sqlite.commit()
+else :
+    i = max(ttt) + 1
+    cur_sqlite.execute(f"""insert into test values ('{i}', '{i}')""")
+    conn_sqlite.commit()
 
 st.write('Hello habagya!')
 st.write(tt)
